d14fb01de39d573be4be4e6e956fdc20ad7e7369
Demyan <demyan.belorurov@gmail.com>
2025-02-13 14:23:09 +0700

    style(PersonList): добавленны комментарии в класс PersonList

--- ClassesPersons/PersonList.cs
+++ ClassesPersons/PersonList.cs
@@ -1,26 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ClassPerson;
+﻿
 
-namespace ClassPersonList
+namespace ClassPerson
 {
+    /// <summary>
+    /// Класс, представляющий список экземпляров Person.
+    /// </summary>
     public class PersonList
     {
+        /// <summary>
+        /// Список экземпляров Person.
+        /// </summary>
         private List<Person> people = new List<Person>();
-        // Метод добавления элементов списка
+
+        /// <summary>
+        /// Метод добавления элементов списка.
+        /// </summary>
         public void AddList(Person person)
         {
             people.Add(person);
         }
-        // Метод удаления элементов списка
+
+        /// <summary>
+        /// Метод удаления элементов списка.
+        /// </summary>     `
         public void RemoveList(Person person)
         {
             people.Remove(person);
         }
-        // Метод удаления элемента по индексу
+
+        /// <summary>
+        /// Метод удаления элемента по индексу.
+        /// </summary>
         public void RemoveIndex(int index)
         {
             if (index < 0 || index >= people.Count)
@@ -29,7 +39,10 @@ namespace ClassPersonList
             }
             people.RemoveAt(index);
         }
-        // Метод получения элемента по индексу
+
+        /// <summary>
+        /// Метод получения элемента по индексу.
+        /// </summary>
         public Person GetIndex(int index)
         {
             if (index >= people.Count || index < 0)
@@ -38,19 +51,31 @@ namespace ClassPersonList
             }
             return people[index];
         }
-        // Метод возвращает индекс элемента в списке
+
+        /// <summary>
+        /// Метод возвращает индекс элемента в списке.
+        /// </summary>
         public int IndexElementFromList(Person person)
         {
             return people.IndexOf(person);
         }
-        // Метод очистки списка
+
+        /// <summary>
+        /// Метод очистки списка.
+        /// </summary>
         public void ClearList()
         {
             people.Clear();
         }
-        // Определение количества элементов в списке
+
+        /// <summary>
+        /// Определение количества элементов в списке.
+        /// </summary>
         public int Count => people.Count;
-        // Метод получения элементов в списке
+
+        /// <summary>
+        /// Метод получения элементов в списке.
+        /// </summary>
         public void PrintList()
         {
             if (people.Count == 0)
