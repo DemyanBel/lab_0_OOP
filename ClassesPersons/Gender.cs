6f7610b0f0323659c36726d9c298aa036b28afae
Demyan <demyan.belorurov@gmail.com>
2025-02-13 14:12:32 +0700

    feature(Person): перечисление добавлено в отдельный файл

---
+++ ClassesPersons/Gender.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassPerson
+{
+    /// <summary>
+    /// Перечисление, представляющее пол человека
+    /// </summary>
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
