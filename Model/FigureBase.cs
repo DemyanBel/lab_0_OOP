32afb3bda18008ddf3e85b1106f57fe15be8d79e
Demyan <demyan.belorurov@gmail.com>
2025-06-05 21:23:07 +0700

    feature(FigureBase): добавлен класс FigureBase

---
+++ Model/FigureBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Model
+{
+    /// <summary>
+    /// Базовый класс 
+    /// для всех объёмных фигур
+    /// </summary>
+    public abstract class FigureBase
+    {
+        /// <summary>
+        /// Свойство для расчёта объёма
+        /// </summary>
+        public abstract double Volume { get; }
+
+        /// <summary>
+        /// Проверка числа
+        /// </summary>
+        /// <param name="number">Число для проверки</param>
+        /// <returns>Корректное число</returns>
+        public static double CheckNumber(double number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Величина должна " +
+                    "быть положительным числом!");
+            }
+            else
+            {
+                return number;
+            }
+        }
+    }
+}
