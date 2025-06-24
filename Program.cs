51a9e00f743e5e28333a6719472423a2696567bc
Demyan <demyan.belorurov@gmail.com>
2025-06-25 05:45:28 +0700

    feature(Program): изменен класс Program

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Lab4
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            //
         }
     }
 }
